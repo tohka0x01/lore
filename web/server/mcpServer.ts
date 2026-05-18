c95f2c1203556f016a7e64093932e20ced5852d4
fattiger <1106842466@qq.com>
2026-05-18 10:58:08 +0800

    release: v1.3.5-pre.2

--- web/server/mcpServer.ts
+++ web/server/mcpServer.ts
@@ -41,7 +41,7 @@ export function createMcpServer(context: McpServerContext = {}): InstanceType<ty
   const server = new McpServer(
     {
       name: 'lore',
-      version: '1.3.5-pre.1',
+      version: '1.3.5-pre.2',
     },
     guidance ? { instructions: guidance } : undefined,
   );
