ab570e6b8d6082c2d2aa3d0de8c33a7e82e5f1ce
fattiger <1106842466@qq.com>
2026-07-19 08:39:39 +0800

    release: v1.3.15-pre.1

--- web/server/mcpServer.ts
+++ web/server/mcpServer.ts
@@ -166,7 +166,7 @@ export async function createMcpServer(context: McpServerContext = {}): Promise<I
   const server = new McpServer(
     {
       name: 'lore',
-      version: '1.3.15-pre.0',
+      version: '1.3.15-pre.1',
     },
     guidance ? { instructions: guidance } : undefined,
   );
